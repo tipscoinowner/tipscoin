58c0a04447078da31b5dfbc6236a482ab0ed99cc
tipscoinowner <249427805+tipscoinowner@users.noreply.github.com>
2026-01-17 11:50:51 +0300

    Delete coin-chain/scripts directory

--- coin-chain/scripts/deploy.ts
+++
@@ -1,9 +0,0 @@
-const TIPSC = await ethers.getContractFactory("TIPSC");
-
-const tipsc = await TIPSC.deploy(
-  await reserveManager.getAddress(),
-  await governance.getAddress(),
-  await treasury.getAddress()
-);
-
-console.log("TIPSC:", await tipsc.getAddress());
