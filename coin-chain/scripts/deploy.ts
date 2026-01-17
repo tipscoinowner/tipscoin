3a1ed5df96dc29a0d9b59345702dd457910a0c72
tipscoinowner <249427805+tipscoinowner@users.noreply.github.com>
2026-01-17 09:51:48 +0300

    Update deploy.ts

--- coin-chain/scripts/deploy.ts
+++ coin-chain/scripts/deploy.ts
@@ -1,1 +1,9 @@
+const TIPSC = await ethers.getContractFactory("TIPSC");
 
+const tipsc = await TIPSC.deploy(
+  await reserveManager.getAddress(),
+  await governance.getAddress(),
+  await treasury.getAddress()
+);
+
+console.log("TIPSC:", await tipsc.getAddress());
