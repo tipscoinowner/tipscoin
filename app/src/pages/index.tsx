38aaf59e9c9ea445c70d0914cb0aa3a45ce43380
tipscoinowner <249427805+tipscoinowner@users.noreply.github.com>
2026-01-17 11:38:35 +0300

    Update index.tsx

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,1 +1,14 @@
+<ValueBanner targetUnitValue={data.targetUnitValue} />
 
+<HealthStatus
+  reserveRatio={data.reserveRatio}
+  isHealthy={data.isHealthy}
+/>
+
+<ReserveInfoModal
+  open={modalOpen}
+  onClose={() => setModalOpen(false)}
+  targetUnitValue={data.targetUnitValue}
+  reserveRatio={data.reserveRatio}
+  minReserveRatio={100}
+/>
